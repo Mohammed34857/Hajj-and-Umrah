ab63cec1e9f09c51043f57ffec43d8e73381ae10
mohammed alfahel <mohammed34857883alfahel@gmail.com>
2024-02-24 01:06:12 +0300

    header

--- src/assets/sections/Header/Header.jsx
+++ src/assets/sections/Header/Header.jsx
@@ -1,11 +1,28 @@
 import './Header.css'
-
+import logo from '../../images/logo.png'
 import React from 'react'
 
-function Header() {
+const Header = () => {
+
+  window.addEventListener('scroll', function() {
+    let navbar = document.getElementById('navbar');
+    let navLinks = document.querySelectorAll('#navbar a');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+      navLinks.forEach(function(link) {
+        link.style.color = 'var(--color-gold)';
+    });
+    } else {
+      navbar.classList.remove('scrolled');
+      navLinks.forEach(function(link) {
+        link.style.color = 'white';
+    });
+    }
+  });
+
   return (
-    <header>
-        <h1> logo</h1>
+    <header id='navbar' >
+        <img src={logo} alt="" />
         <nav>
             <ul>
                 <li><a href="#">الصفحة الرئيسية</a></li>
