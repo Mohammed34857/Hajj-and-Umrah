cf56a8d5d6dddf33a5a8414a87429eadb5aad17f
SHIMOO\User <makkishimoo9@gmail.com>
2024-03-09 23:56:06 +0300

    update footer

--- src/assets/sections/Footer/Footer.jsx
+++ src/assets/sections/Footer/Footer.jsx
@@ -1,20 +1,63 @@
-import './Footer.css'
+import "./Footer.css";
 
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
     <footer>
-        <div className="container">
-         <div className="row">
+      <div className="parent">
+        <div className="address">
+          <ul>
+            <h2>address</h2>
+            <li>
+              <i className="fa fa-location"></i>location
+            </li>
+            <li>call +963 993642776</li>
+            <li>demo@gmail.com</li>
+            <li>
+              <i></i>
+              <i></i>
+              <i></i>
+            </li>
+          </ul>
+        </div>
+        <div className="info">
+          <h2>info</h2>
+          <p>through our superior services and international parenter <br />we offer you multiplr options for program to visit the two holy <br />
+          mosgues in mahwi al-afida.</p>
+        </div>
+        <div className="links">
+          <h2>links</h2>
+          <ul className="ul-two">
+                <li><a href="">Home</a></li>
+                <li><a href="#">Hajj Programs</a></li>
+                <li><a href="#">Umrah programs</a></li>
+                <li><a href="#">Hotels</a></li>
+                <li><a href="#">Transport</a></li>
+            </ul>
+        </div>
+        <div className="subscribe"> <h2>subscribe</h2>
+        <input type="email" placeholder="enter email" />
+        <br />
+        <div className="line"></div>
+        <button>subscribe</button>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
           <div className="col-lg-12">
-           <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved. 
-            <br/>Design: <a href="#">TemplateMo</a></p>
+            <p className="p-2">
+              Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights
+              reserved.
+              <br />
+              Design: <a href="#">TemplateMo</a>
+            </p>
           </div>
-         </div>
-         </div>
-      </footer>
-  )
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
