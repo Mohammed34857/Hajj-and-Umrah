f14237135963d859db98a7a194f875984049a91c
mohammed alfahel <mohammed34857883alfahel@gmail.com>
2024-03-12 08:36:19 +0300

    response the page UmrahProgras

--- src/assets/components/HotelBusInUmrahProgram/HotelBusInUmrahProgram.jsx
+++ src/assets/components/HotelBusInUmrahProgram/HotelBusInUmrahProgram.jsx
@@ -4,19 +4,21 @@ import './HotelBusInUmrahProgram.css'
 const HotelBusInUmrahProgram = (props) => {
   return (
     <div className='HotelBusInUmrahProgram'>
-         <div className='col' style={{ backgroundImage: `url(${props.image1})` }}>
+         <div className='colum1'>
+         <div className='col' id='col1' style={{ backgroundImage: `url(${props.image1})` }}>
                <div className='content'>
                  <h1>{props.HotelName1}</h1>
                  <p>{props.location1}</p>
               </div>
           </div>
-          <div className='col' id='col2' style={{ backgroundImage: `url(${props.image2})` }}>
+          <div className='col'  style={{ backgroundImage: `url(${props.image2})` }}>
               <div className='content'>
                  <h1>{props.HotelName2}</h1>
                  <p>{props.location2}</p>
               </div>
             </div>
-          <div id='col3'>
+         </div>
+          <div className='colum2'>
               <div className='col' style={{ backgroundImage: `url(${props.image3})` }}>
                  <div className='content'>
                    <h1>{props.HotelName3}</h1>
@@ -25,8 +27,8 @@ const HotelBusInUmrahProgram = (props) => {
                </div>
               <div className='col' style={{ backgroundImage: `url(${props.image4})` }}>
                  <div className='content'>
-                   <h1>{props.HotelName1}</h1>
-                   <p>{props.location1}</p>
+                   <h1>{props.HotelName4}</h1>
+                   <p>{props.location4}</p>
                  </div>
               </div>
           </div>
